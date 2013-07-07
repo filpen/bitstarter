a532367c019ceabeb41bb24ee793653439d1ecb0
Filippo Pensalfini <filippo.pensalfini@gmail.com>
2013-07-07 10:56:31 +0200

    Updated grader.js to support URLs and extended checks.json

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -34,6 +35,10 @@ var checkHtmlFile = function(htmlFile, checksFile) {
     return out;
 };
 
+var checkUrl = function(url, checksFile) {
+    rest.get(url).on('complete', checkHtmlFile);
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -44,9 +49,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url for index.html')
         .parse(process.argv);
 
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson;
+    if(program.file) {
+        checkJson = checkHtmlFile(program.file, program.checks);
+    } else {
+        checkJson = checkUrl(program.url, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
